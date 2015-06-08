7c470c6469dc066d799fb49e7cf779331139811c
eliasvansickle <eliasvansickle@gmail.com>
2015-06-08 16:26:23 -0700

    "Button hover on, hover off working well"

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-	
+	var click = false
+	$("button").hover(function(){
+		var reveal = $(this).attr("data-alt-src");
+		$("#wrapper").css("background-image", "url("+reveal+")");
+	},
 
+	function() {
+		if (!click) {
+		$("#wrapper").css("background-image", "none");
+	}});
+
+	$("button").click(function() {
+		click = true;
+		var choice = $(this).attr("data-alt-src");
+		$("#wrapper").css("background-image", "url("+choice+")");
+		$("#selection").hide();
+	});
+	
 });
